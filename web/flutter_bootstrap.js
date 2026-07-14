84fd8de176e88014974ea12f9b18bcc971dcb255
OmarElsharkawy <o.elsharkawy@wecodeforyou.io>
2026-07-14 14:20:20 +0300

    1.0.4+27

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1940072460" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2817615153" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
